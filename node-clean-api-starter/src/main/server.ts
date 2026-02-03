fc02b8c06c6b4538695e54f19f410de61459358f
Matheus Gomes <matheus081105@gmail.com>
2026-02-03 16:51:00 -0300

    add dotenv library for env

--- node-clean-api-starter/src/main/server.ts
+++ node-clean-api-starter/src/main/server.ts
@@ -1,4 +1,7 @@
 import { app } from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // sempre legal deixar a porta como variável de ambiente, eu pelo menos prefiro assim :)
 const PORT = process.env.PORT || 3000;
